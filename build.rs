eca8ef4325bc0d45feccd3efaf008d215ddedcc1
secona <77039267+secona@users.noreply.github.com>
2025-05-05 09:00:39 +0700

    feat: create base skeleton of Rust gRPC

---
+++ build.rs
@@ -0,0 +1,10 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .compile(
+            &["proto/services.proto"],
+            &["proto"],
+        )?;
+
+    Ok(())
+}
